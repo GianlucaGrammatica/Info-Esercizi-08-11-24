ca378651e1e8b9fa62a40cc012930ad06ebcb5cd
Gianluca <10934204@itisrossi.vi.it>
2024-11-08 08:59:54 +0100

    Aggiungere i file di progetto.

---
+++ Info-Esercizi 08-11-24/Es1-Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info_Esercizi_08_11_24
+{
+    internal class Es1Task
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Info-Esercizi 08-11-24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info_Esercizi_08_11_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
